f9caeefbbe9d6fee1500a52374a119761a005008
Tiana <Tiana.marie013@gmail.com>
2021-02-17 20:18:32 -0700

    component build

--- App.js
+++ App.js
@@ -5,7 +5,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!!</Text>
+      <Text>
+        Hello whirld
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
