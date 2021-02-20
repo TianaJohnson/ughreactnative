4dc7f25fc1a29f8c2979a35dfd04403ffa53c881
Tiana <Tiana.marie013@gmail.com>
2021-02-19 18:59:33 -0700

    button styles

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet,
+         Text,
+         View,
+         TextInput,
+         Button,
+         } from 'react-native';
 
 import Hello from './Hello';
 
@@ -36,6 +41,20 @@ render() {
         placeholder="0.00"
         onChangeText={(text) => this.setState({inputValue: text})}
         />
+        <View style={styles.buttonGroup}>
+          <Button
+            title="10%"
+            onPress={() => this.setState({ tip: 0.1 })}
+           />
+           <Button
+            title="20%"
+            onPress={() => this.setState({ tip: 0.2 })}
+           />
+           <Button
+            title="25%"
+            onPress={() => this.setState({ tip: 0.25 })}
+           />
+        </View>
     </View>
   );
 }
@@ -57,5 +76,9 @@ const styles = StyleSheet.create({
     borderColor: '#333',
     borderWidth: 1,
     padding: 5,
+  },
+  buttonGroup: {
+    flexDirection: 'row'
   }
+
 });
